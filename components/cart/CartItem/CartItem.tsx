9e40d0a065421a1f8d4766fa7116f96c371c456b
Cl2am3l <57160434@go.buu.ac.th>
2021-02-09 00:47:27 +0700

    insert cart

---
+++ components/cart/CartItem/CartItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const CartItem = (props: Props) => {
+    return (
+        <div>
+            cartitem
+        </div>
+    )
+}
+
+export default CartItem
---
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const CartSidebarView = (props: Props) => {
+    return (
+        <div>
+            CartSidebarView
+        </div>
+    )
+}
+
+export default CartSidebarView
